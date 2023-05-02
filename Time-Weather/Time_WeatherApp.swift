3cb6ca32e7c768943339b6ef2c5e93542a1a4001
HugoLDev <hugolope30@icloud.com>
2023-05-02 14:00:34 -0300

    First Commit

---
+++ Time-Weather/Time_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_WeatherApp.swift
+//  Time-Weather
+//
+//  Created by Hugo Lopes Mendonca on 08/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Time_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
